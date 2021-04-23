3fe80babce53c4b8bf377ffa289e5b2b7f3acef6
i958047527992 <i90681472a@gmail.com>
2021-04-23 10:55:58 +0800

    week2 homework finished

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,20 @@
 function join(arr, concatStr) {
-  
+    var new_str=''
+    for(var i=0;i<arr.length-1;i++){
+        new_str += arr[i]
+        new_str += concatStr
+    }
+    new_str += arr[arr.length-1]
+    return new_str
 }
 
 function repeat(str, times) {
-  
+    var new_str=''
+    for(var i=0;i<times;i++){
+        new_str += str
+    }
+    return new_str
 }
 
 console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+
